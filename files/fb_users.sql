465911a70a35018c4a4f3851f487202309b96857
Muhammad Nouman ANjum <nouman92@live.com>
2015-05-31 12:16:58 -0700

    db updated
    updated db

--- files/fb_users.sql
+++ files/fb_users.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 26, 2015 at 10:42 AM
+-- Generation Time: May 31, 2015 at 09:13 PM
 -- Server version: 5.5.32
 -- PHP Version: 5.4.16
 
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS `functions` (
   KEY `UID` (`UID`),
   KEY `UID_2` (`UID`),
   KEY `UID_3` (`UID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
 
 --
 -- Dumping data for table `functions`
@@ -48,7 +48,10 @@ INSERT INTO `functions` (`fid`, `UID`, `function`, `private`, `name`) VALUES
 (8, 1, 'add(a,b,c,d[66])\n{\nreturn a+b+c;\n}', 0, 'add(a,b,s,d[66])'),
 (9, 4, 'add(a,b,c,d[])\n{\nreturn a+b+c;\n}', 0, 'add(a,b,c,d[])'),
 (10, 4, 'sum (a,b)\n{\n  return a+b;\n}\n', 0, 'sum(a,b)'),
-(11, 4, 'randome(e)\n{\nreturn e;\n}', 0, '');
+(11, 4, 'randome(e)\n{\nreturn e;\n}', 0, ''),
+(12, 4, 'dlete(a,b)\n{\nreturn a-b;\n}', 0, ''),
+(13, 11, 'sum(w)\n{\nreturn w;\n}', 0, ''),
+(14, 11, 'sum(e)\n{\nreturn e;\n}', 0, '');
 
 -- --------------------------------------------------------
 
@@ -70,7 +73,10 @@ INSERT INTO `inputs` (`fid`, `input`) VALUES
 (8, '1 ;7 ;2 ;array:{8,2,8,2,5,6,1,5,7,5,2,3,5,1,6,5,10,7,8,3,9,6,3,7,3,9,8,9,5,7,1,10,5,6,4,8,9,9,10,6,6,7,5,6,10,9,1,1,4,2,4,6,5,9,2,7,4,2,4,3,8,9,2,8,9,5} ;'),
 (9, '1 ;4 ;2 ;array:{9,2,10,1,8,7,5,2} ;'),
 (10, '3 ;4 ;'),
-(11, '4 ;');
+(11, '4 ;'),
+(12, '8;9;'),
+(13, '1;'),
+(14, '8;');
 
 -- --------------------------------------------------------
 
@@ -92,7 +98,10 @@ INSERT INTO `outputs` (`fid`, `output`) VALUES
 (8, '10'),
 (9, '7'),
 (10, '7'),
-(11, '4');
+(11, '4'),
+(12, '-1'),
+(13, '1'),
+(14, '8');
 
 -- --------------------------------------------------------
 
@@ -127,7 +136,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `Femail` varchar(60) DEFAULT NULL,
   `password` varchar(15) NOT NULL,
   PRIMARY KEY (`UID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
 --
 -- Dumping data for table `users`
@@ -142,7 +151,10 @@ INSERT INTO `users` (`UID`, `Fuid`, `Ffname`, `Femail`, `password`) VALUES
 (6, '75631605390374', 'NOUMn', 'nouman92', 'nouman92'),
 (7, '96479268999294', 'NOUMn', 'nouman92', 'nouman92'),
 (8, '93490847722252', 'nouman92', 'nouman92', 'nouman92'),
-(9, '98939723341441', 'nouman92', 'nouman92', 'nouman92');
+(9, '98939723341441', 'nouman92', 'nouman92', 'nouman92'),
+(10, '13275910838487', 'muhammad nouman anjum', 'nouman_anjum@ymail.com', 'noumananjum'),
+(11, '16190910437223', 'nouman anjum', 'nouman92@live.com', 'nouman92'),
+(12, '89240360576880', 'nouman', 'nouman92@live.com', '1');
 
 --
 -- Constraints for dumped tables
